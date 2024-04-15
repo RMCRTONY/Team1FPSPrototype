5464b9e4a9986d5d322fb39a64a9c458729a2a9b
Avery James <Ajlegogold@gmail.com>
2024-04-15 15:46:53 -0400

    Dash movement option
    The player can now dash. Movement is not conserved for any period of time.

--- PP2 Team 1 FPS Prototype/Assets/Scripts/playerController.cs
+++ PP2 Team 1 FPS Prototype/Assets/Scripts/playerController.cs
@@ -13,6 +13,10 @@ public class playerController : MonoBehaviour, IDamage // needs IInteractions
     [SerializeField] int jumpSpeed;
     [SerializeField] int gravityMultiplier; // IMPORTANT: gravity is negative downward force
     [SerializeField] int maxJumps;
+    [SerializeField] int dashSpeed;
+    [SerializeField] float dashRate;
+    [SerializeField] float dashTime;
+    [SerializeField] float dashCooldown;
 
     // the firing values
     [SerializeField] int spellDamage;
@@ -23,8 +27,10 @@ public class playerController : MonoBehaviour, IDamage // needs IInteractions
     private Vector3 playerVel;
     private int jumpTimes;
     private bool isShooting;
+    private bool isDashing;
+    private bool canDash = true;
 
-    private int gravity = -10;
+    private readonly int gravity = -10;
 
     // Start is called before the first frame update
     void Start()
@@ -43,8 +49,8 @@ public class playerController : MonoBehaviour, IDamage // needs IInteractions
 
     void movement()
     {
-        // onground check for jumps && vel.y for upward movement
-        if (controller.isGrounded && playerVel.y < 0)
+        // onground check for jumps && vel.y for upward movement or a dash
+        if ((controller.isGrounded && playerVel.y < 0) || isDashing)
         {
             jumpTimes = 0;
             playerVel = Vector3.zero;
@@ -55,13 +61,19 @@ public class playerController : MonoBehaviour, IDamage // needs IInteractions
         }
 
         // tie movement to player axis (vector addition)
-        moveDir = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
-        controller.Move(moveDir * walkSpeed * Time.deltaTime);
+        moveDir = getDirection();
+        controller.Move(walkSpeed * Time.deltaTime * moveDir);
 
         // let the man KILL, damn you
         if (Input.GetButton("Fire1") && !isShooting && !gameManager.instance.isPaused)
         {
             StartCoroutine(castSpell());
+        }
+
+        // check for dash key, make dash happen
+        if (Input.GetButtonDown("Fire3") && canDash && !gameManager.instance.isPaused)
+        {
+            StartCoroutine(dash());
         }
 
         // check for jump key, make jump happen
@@ -71,7 +83,7 @@ public class playerController : MonoBehaviour, IDamage // needs IInteractions
             playerVel.y = jumpSpeed;
         }
 
-        controller.Move(playerVel * Time.deltaTime);
+        controller.Move(playerVel * Time.deltaTime); // input the jump
     }
 
     IEnumerator castSpell() // eventually recieve an enum that indicates kind of spell
@@ -79,8 +91,7 @@ public class playerController : MonoBehaviour, IDamage // needs IInteractions
         isShooting = true;
 
         // TODO: fire a projectile, eventually with variable attributes
-        RaycastHit hit;
-        if (Physics.Raycast(Camera.main.ViewportPointToRay(new Vector2(0.5f, 0.5f)), out hit, spellDist))
+        if (Physics.Raycast(Camera.main.ViewportPointToRay(new Vector2(0.5f, 0.5f)), out RaycastHit hit, spellDist))
         {
             IDamage dmg = hit.collider.GetComponent<IDamage>();
 
@@ -94,15 +105,41 @@ public class playerController : MonoBehaviour, IDamage // needs IInteractions
         isShooting = false;
     }
 
+    IEnumerator dash()
+    {
+        isDashing = true;
+        canDash = false;
+
+        // add dash speed to proper vector direction
+        float startTime = Time.time;
+        while (Time.time < startTime + dashTime) 
+        { 
+            controller.Move(dashSpeed * Time.deltaTime * moveDir); // input the dash
+            yield return null;
+        }
+        
+        yield return new WaitForSeconds(dashRate);
+        isDashing = false;
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
+    }
+
     public void takeDamage(int amount)
     {
         // take da health away
         HP -= amount;
 
         if (HP <= 0) 
-        { 
-            // TODO: you lose, loser
-
+        {
+            // you lose, loser
+            gameManager.instance.youLose();
         }
+    }
+
+    private Vector3 getDirection()
+    {
+        // orient inputs
+        Vector3 dir = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
+        return dir;
     }
 }
