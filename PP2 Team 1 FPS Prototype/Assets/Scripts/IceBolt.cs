e397bca1f63ed7a47bb9d3709ccb300f507fbb98
RmcrTony <Ajlegogold@gmail.com>
2024-05-13 21:24:17 -0400

    Slow/freeze
    The beginning of an attempt at implementing some ice wand functionality. I fell into a research pit and need to revisit this.

---
+++ PP2 Team 1 FPS Prototype/Assets/Scripts/IceBolt.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceBolt : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+
+    [SerializeField] int damage;
+    [SerializeField] int speed;
+    [SerializeField] int destroyTime;
+
+    bool hitHappened;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.forward * speed;
+        Destroy(gameObject, destroyTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.isTrigger)
+            return;
+
+        IDamage dmg = other.GetComponent<IDamage>();
+
+        if (dmg != null && !hitHappened)
+        {
+            dmg.takeDamage(damage);
+            hitHappened = true;
+        }
+
+        Destroy(gameObject);
+    }
+}
