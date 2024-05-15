6a392532d0957f42623c93a26145f30b9f1a3d71
Payton-Coburn <132382740+Payton-Coburn@users.noreply.github.com>
2024-05-15 18:05:48 -0400

    Load scene
    Created loading screen

--- PP2 Team 1 FPS Prototype/Assets/Scripts/nextScene.cs
+++ PP2 Team 1 FPS Prototype/Assets/Scripts/nextScene.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class nextScene : MonoBehaviour
 {
+    public GameObject LoaderUI;
+    public Slider progressSlider;
     int sceneID;
 
     private void Update()
@@ -16,7 +19,34 @@ public class nextScene : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadSceneAsync(sceneID + 1);
+            SceneManager.LoadSceneAsync(1);
+        }
+    }
+
+    public void LoadScene(int index)
+    {
+        StartCoroutine(LoadScene_Routine(index));
+    }
+
+    public IEnumerator LoadScene_Routine(int index)
+    {
+        progressSlider.value = 0;
+        LoaderUI.SetActive(true);
+
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(index);
+        asyncOperation.allowSceneActivation = false;
+        float progress = 0;
+        while (!asyncOperation.isDone)
+        {
+            progress = Mathf.MoveTowards(progress, asyncOperation.progress, Time.deltaTime);
+            progressSlider.value = progress;
+            if(progress >= 0.9f)
+            {
+                progressSlider.value = 1;
+                asyncOperation.allowSceneActivation = true;
+            }
+
+            yield return null;
         }
     }
 }
